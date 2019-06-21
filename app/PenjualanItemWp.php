d4f9a1dd8be64b95b97bbb48cbe2ad8cf613b3c8
Bagas Udi Sahsangka <udibagas@gmail.com>
2019-06-21 16:48:04 +0700

    add itemwp on penjulan

---
+++ app/PenjualanItemWp.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PenjualanItemWp extends Model
+{
+    protected $fillable = [
+        'penjualan_id', 'material_id', 'material_description',
+        'price_per_unit', 'value', 'berat'
+    ];
+
+    protected $casts = [
+        'penjualan_id' => 'integer',
+        'price_per_unit' => 'integer',
+        'value' => 'integer',
+        'berat' => 'float',
+    ];
+}
