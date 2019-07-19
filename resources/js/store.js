bad506d963153b8615465135f5aa4752db9a0ca4
Bagas Udi Sahsangka <udibagas@gmail.com>
2019-07-19 16:16:28 +0700

    jos

--- resources/js/store.js
+++ resources/js/store.js
@@ -5,12 +5,6 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
-        pagerOptions: [
-            { value: 10, label: 10 },
-            { value: 25, label: 25 },
-            { value: 50, label: 50 },
-            { value: 100, label: 100 }
-        ],
         kategoriBarangList: [],
         pengeluaranList: [],
         locationList: [],
