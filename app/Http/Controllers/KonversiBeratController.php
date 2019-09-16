5a505c093a4cb8c9cce1d1275ac9aac35c1e2785
Bagas Udi Sahsangka <udibagas@gmail.com>
2019-09-16 11:22:32 +0700

    add crud on konversi berat

--- app/Http/Controllers/KonversiBeratController.php
+++ app/Http/Controllers/KonversiBeratController.php
@@ -22,29 +22,65 @@ class KonversiBeratController extends Controller
             return $q->where('material_id', 'LIKE', '%' . $request->keyword . '%')
                     ->orWhere('material_description', 'LIKE', '%' . $request->keyword . '%')
                     ->orWhere('finished_good', 'LIKE', '%' . $request->keyword . '%')
-                    ->orWhere('kategori_jual', 'LIKE', '%' . $request->keyword . '%');
+                    ->orWhere('kategori_jual', 'LIKE', '%' . $request->keyword . '%')
+                    ->orWhere('keterangan', 'LIKE', '%' . $request->keyword . '%');
         })->orderBy($sort, $order)->paginate($request->pageSize);
     }
 
     public function store(Request $request)
     {
-        try {
-            DB::beginTransaction();
-            foreach ($request->rows as $row) {
-                    $konversi = KonversiBerat::where('material_id', $row['material_id'])->first();
+        // input batch
+        if ($request->rows)
+        {
+            try {
+                DB::beginTransaction();
+                foreach ($request->rows as $row) {
+                        $konversi = KonversiBerat::where('material_id', $row['material_id'])->first();
 
-                    if ($konversi) {
-                        DB::table('konversi_berats')->where('id', $konversi->id)->update($row);
-                    } else {
-                        DB::table('konversi_berats')->insert($row);
+                        if ($konversi) {
+                            DB::table('konversi_berats')->where('id', $konversi->id)->update($row);
+                        } else {
+                            DB::table('konversi_berats')->insert($row);
+                        }
                     }
-                }
 
-            DB::commit();
-            return 'Data imported successfully. You can close this window or uplad more files.<br>';
-        } catch (\Exception $e) {
-            DB::rollBack();
-            return '[ERROR] Failed to import data. ' . $e->getMessage().'<br> ' ;
+                DB::commit();
+                return 'Data imported successfully. You can close this window or uplad more files.<br>';
+            } catch (\Exception $e) {
+                DB::rollBack();
+                return '[ERROR] Failed to import data. ' . $e->getMessage().'<br> ' ;
+            }
         }
+
+        $request->validate([
+            'kategori_jual' => 'required',
+            'finished_good' => 'required',
+            'material_id' => 'required',
+            'material_description' => 'required',
+            'berat' => 'required|numeric'
+        ]);
+
+        $konversiBerat = KonversiBerat::create($request->all());
+        return $konversiBerat;
+    }
+
+    public function update(KonversiBerat $konversiBerat, Request $request)
+    {
+        $request->validate([
+            'kategori_jual' => 'required',
+            'finished_good' => 'required',
+            'material_id' => 'required',
+            'material_description' => 'required',
+            'berat' => 'required|numeric'
+        ]);
+
+        $konversiBerat->update($request->all());
+        return $konversiBerat;
+    }
+
+    public function destroy(KonversiBerat $konversiBerat)
+    {
+        $konversiBerat->delete();
+        return ['message' => 'Data berhasil dihapus'];
     }
 }
--- resources/js/pages/KonversiBerat.vue
+++ resources/js/pages/KonversiBerat.vue
@@ -7,6 +7,9 @@
             <el-form-item>
                 <el-button @click="openForm" type="primary" icon="el-icon-upload">UPLOAD DATA KONVERSI BERAT</el-button>
             </el-form-item>
+            <el-form-item>
+                <el-button @click="() => { showForm = true; formModel = {}; }" type="primary" icon="el-icon-plus">TAMBAH KONVERSI BERAT</el-button>
+            </el-form-item>
             <el-form-item style="margin-right:0;">
                 <el-input placeholder="Search" prefix-icon="el-icon-search" v-model="keyword" @change="requestData" clearable>
                     <el-button @click="() => { page = 1; keyword = ''; requestData(); }" slot="append" icon="el-icon-refresh"></el-button>
@@ -27,6 +30,19 @@
             <el-table-column prop="material_description" min-width="350" label="Material Description" sortable="custom"></el-table-column>
             <el-table-column prop="berat" label="Berat Rata - Rata (gr)" align="center" header-align="center" sortable="custom" min-width="170px"></el-table-column>
             <el-table-column prop="keterangan" label="Keterangan" sortable="custom" min-width="150px"></el-table-column>
+            <el-table-column fixed="right" width="40px">
+                <template slot-scope="scope">
+                    <el-dropdown>
+                        <span class="el-dropdown-link">
+                            <i class="el-icon-more"></i>
+                        </span>
+                        <el-dropdown-menu slot="dropdown">
+                            <el-dropdown-item @click.native.prevent="() => { showForm = true; formModel = JSON.parse(JSON.stringify(scope.row)); }"><i class="el-icon-edit-outline"></i> Edit</el-dropdown-item>
+                            <el-dropdown-item @click.native.prevent="deleteData(scope.row.id)"><i class="el-icon-delete"></i> Hapus</el-dropdown-item>
+                        </el-dropdown-menu>
+                    </el-dropdown>
+                </template>
+            </el-table-column>
         </el-table>
 
         <br>
@@ -40,6 +56,56 @@
         :page-sizes="[10,25,50,100]"
         :total="paginatedData.total">
         </el-pagination>
+
+        <el-dialog center :visible.sync="showForm" :title="!!formModel.id ? 'EDIT KONVERSI BERAT' : 'TAMBAH KONVERSI BERAT'" width="450px" v-loading="loading" :close-on-click-modal="false">
+            <el-alert type="error" title="ERROR"
+                :description="error.message + '\n' + error.file + ':' + error.line"
+                v-show="error.message"
+                style="margin-bottom:15px;">
+            </el-alert>
+
+            <el-form label-width="150px" label-position="left">
+                <el-form-item label="Kategori Jual" :class="formErrors.kategori_jual ? 'is-error' : ''">
+                    <el-select placeholder="Kategori Jual" v-model="formModel.kategori_jual" style="width:100%;">
+                        <el-option
+                        v-for="k in $store.state.kategoriBarangList.filter(k => k.jenis == 'WP')"
+                        :key="k.id"
+                        :value="k.nama"
+                        :label="k.nama"></el-option>
+                    </el-select>
+                    <div class="el-form-item__error" v-if="formErrors.kategori_jual">{{formErrors.kategori_jual[0]}}</div>
+                </el-form-item>
+
+                <el-form-item label="Finished Goods" :class="formErrors.finished_good ? 'is-error' : ''">
+                    <el-input placeholder="Finished Goods" v-model="formModel.finished_good"></el-input>
+                    <div class="el-form-item__error" v-if="formErrors.finished_good">{{formErrors.finished_good[0]}}</div>
+                </el-form-item>
+
+                <el-form-item label="Material ID" :class="formErrors.material_id ? 'is-error' : ''">
+                    <el-input placeholder="Material ID" v-model="formModel.material_id"></el-input>
+                    <div class="el-form-item__error" v-if="formErrors.material_id">{{formErrors.material_id[0]}}</div>
+                </el-form-item>
+
+                <el-form-item label="Material Description" :class="formErrors.material_description ? 'is-error' : ''">
+                    <el-input placeholder="Material Description" v-model="formModel.material_description"></el-input>
+                    <div class="el-form-item__error" v-if="formErrors.material_description">{{formErrors.material_description[0]}}</div>
+                </el-form-item>
+
+                <el-form-item label="Berat Rata - Rata (gr)" :class="formErrors.berat ? 'is-error' : ''">
+                    <el-input type="number" placeholder="Berat Rata - Rata (gr)" v-model="formModel.berat"></el-input>
+                    <div class="el-form-item__error" v-if="formErrors.berat">{{formErrors.berat[0]}}</div>
+                </el-form-item>
+
+                <el-form-item label="Keterangan">
+                    <el-input placeholder="Keterangan" v-model="formModel.keterangan"></el-input>
+                </el-form-item>
+            </el-form>
+
+            <span slot="footer" class="dialog-footer">
+                <el-button type="primary" @click="() => !!formModel.id ? update() : store()" icon="el-icon-success">SAVE</el-button>
+                <el-button type="info" @click="showForm = false" icon="el-icon-error">CANCEL</el-button>
+            </span>
+        </el-dialog>
 
         <el-dialog
         :visible.sync="uploadFormDialog"
@@ -70,7 +136,11 @@ export default {
             order: 'ascending',
             paginatedData: {},
             uploadFormDialog: false,
-            logs: ['Please select file...<br>']
+            logs: ['Please select file...<br>'],
+            formModel: {},
+            formErrors: {},
+            error: {},
+            showForm: false
         }
     },
     methods: {
@@ -78,6 +148,74 @@ export default {
             $('#file-upload').val('');
             this.logs = ['Please select file...<br>']
             this.uploadFormDialog = true
+        },
+        deleteData(id) {
+            this.$confirm('Anda yakin akan menghapus data ini?', 'Warning', {
+                type: 'warning',
+                confirmButtonText: 'Ya',
+                cancelButtonText: 'Tidak'
+            }).then(() => {
+                axios.delete('/konversiBerat/' + id).then(r => {
+                    this.requestData();
+                    this.$message({
+                        message: 'Data berhasil dihapus.',
+                        type: 'success',
+                        showClose: true
+                    });
+                }).catch(e => {
+                    this.$message({
+                        message: 'Data gagal dihapus. ' + e.response.data.message,
+                        type: 'error',
+                        showClose: true
+                    });
+                })
+            }).catch(e => console.log(e));
+        },
+        store() {
+            this.loading = true;
+            axios.post('/konversiBerat', this.formModel).then(r => {
+                this.showForm = false;
+                this.$message({
+                    message: 'Data berhasil disimpan.',
+                    type: 'success'
+                });
+                this.requestData();
+            }).catch(e => {
+                if (e.response.status == 422) {
+                    this.error = {}
+                    this.formErrors = e.response.data.errors;
+                }
+
+                if (e.response.status == 500) {
+                    this.formErrors = {}
+                    this.error = e.response.data;
+                }
+            }).finally(() => {
+                this.loading = false
+            })
+        },
+        update() {
+            this.loading = true;
+            axios.put('/konversiBerat/' + this.formModel.id, this.formModel).then(r => {
+                this.showForm = false
+                this.$message({
+                    message: 'Data berhasil disimpan.',
+                    type: 'success'
+                });
+                this.requestData()
+            }).catch(e => {
+                if (e.response.status == 422) {
+                    this.error = {}
+                    this.formErrors = e.response.data.errors;
+                }
+
+                if (e.response.status == 500) {
+                    this.formErrors = {}
+                    this.error = e.response.data;
+                }
+            }).finally(() => {
+                this.loading = false
+            })
         },
         save(data) {
             // console.log(data)
@@ -194,6 +332,7 @@ export default {
     },
     mounted() {
         this.requestData();
+        this.$store.commit('getKategoriBarangList')
         let _this = this
         $('body').on('change', '#file-upload', function(ev) {
             _this.readFile(ev);
