51c3a8728f1d5d7f614615045b3dad340e98a017
Bagas Udi Sahsangka <bagas@lamsolusi.com>
2020-09-17 10:21:16 +0700

    update

--- resources/js/components/PenjualanDetailWp.vue
+++ resources/js/components/PenjualanDetailWp.vue
@@ -1,145 +1,183 @@
 <template>
-    <div>
-        <el-row :gutter="15">
-            <el-col :span="12">
-                <table class="table table-sm table-bordered">
-                    <tbody>
-                        <tr><td class="td-label">Tanggal</td><td class="td-value">{{data.tanggal | readableDate}}</td></tr>
-                        <tr><td class="td-label">No. Surat Jalan</td><td class="td-value">{{data.no_sj}}</td></tr>
-                        <tr><td class="td-label">No. Pengajuan</td><td class="td-value">{{data.no_aju}}</td></tr>
-                        <tr><td class="td-label">User</td><td class="td-value">{{data.user}}</td></tr>
-                        <tr><td class="td-label">Plant</td><td class="td-value">{{data.location.plant}} - {{data.location.name}}</td></tr>
-                        <tr><td class="td-label">Pembeli</td><td class="td-value">{{data.pembeli.nama}} - {{data.pembeli.kontak}}</td></tr>
-                        <tr><td class="td-label">TOP Date</td><td class="td-value">{{data.top_date | readableDate}}</td></tr>
-                    </tbody>
-                </table>
-            </el-col>
-            <el-col :span="12">
-                <table class="table table-sm table-bordered">
-                    <tbody>
-                        <tr><td class="td-label">Jembatan Timbang</td><td class="td-value">{{data.jembatan_timbang | formatNumber}} Kg</td></tr>
-                        <tr><td class="td-label">Value</td><td class="td-value">Rp {{data.value | formatNumber}}</td></tr>
-                        <tr><td class="td-label">Terbayar</td><td class="td-value">Rp {{data.terbayar | formatNumber}}</td></tr>
-                        <tr><td class="td-label">Outstanding</td><td class="td-value">Rp {{data.value - data.terbayar | formatNumber}}</td></tr>
-                        <tr>
-                            <td class="td-label">Status</td>
-                            <td class="td-value">
-                                <el-tag size="small" :type="statuses[data.status].type">{{statuses[data.status].label}}</el-tag>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td class="td-label"> Status Pembayaran</td>
-                            <td class="td-value">
-                                <el-tag size="small" :type="statuses_pembayaran[data.status_pembayaran].type">
-                                    {{statuses_pembayaran[data.status_pembayaran].label}}
-                                </el-tag>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </el-col>
-        </el-row>
+	<div>
+		<el-row :gutter="15">
+			<el-col :span="12">
+				<table class="table table-sm table-bordered">
+					<tbody>
+						<tr>
+							<td class="td-label">Tanggal</td>
+							<td class="td-value">{{data.tanggal | readableDate}}</td>
+						</tr>
+						<tr>
+							<td class="td-label">No. Surat Jalan</td>
+							<td class="td-value">{{data.no_sj}}</td>
+						</tr>
+						<tr>
+							<td class="td-label">No. Pengajuan</td>
+							<td class="td-value">{{data.no_aju}}</td>
+						</tr>
+						<tr>
+							<td class="td-label">User</td>
+							<td class="td-value">{{data.user}}</td>
+						</tr>
+						<tr>
+							<td class="td-label">Plant</td>
+							<td class="td-value">{{data.location.plant}} - {{data.location.name}}</td>
+						</tr>
+						<tr>
+							<td class="td-label">Pembeli</td>
+							<td class="td-value">{{data.pembeli.nama}} - {{data.pembeli.kontak}}</td>
+						</tr>
+						<tr>
+							<td class="td-label">TOP Date</td>
+							<td class="td-value">{{data.top_date | readableDate}}</td>
+						</tr>
+					</tbody>
+				</table>
+			</el-col>
+			<el-col :span="12">
+				<table class="table table-sm table-bordered">
+					<tbody>
+						<tr>
+							<td class="td-label">Jembatan Timbang</td>
+							<td class="td-value">{{data.jembatan_timbang | formatNumber}} Kg</td>
+						</tr>
+						<tr>
+							<td class="td-label">Value</td>
+							<td class="td-value">Rp {{data.value | formatNumber}}</td>
+						</tr>
+						<tr>
+							<td class="td-label">Terbayar</td>
+							<td class="td-value">Rp {{data.terbayar | formatNumber}}</td>
+						</tr>
+						<tr>
+							<td class="td-label">Outstanding</td>
+							<td class="td-value">Rp {{data.value - data.terbayar | formatNumber}}</td>
+						</tr>
+						<tr>
+							<td class="td-label">Status</td>
+							<td class="td-value">
+								<el-tag size="small" :type="statuses[data.status].type">{{statuses[data.status].label}}</el-tag>
+							</td>
+						</tr>
+						<tr>
+							<td class="td-label">Status Pembayaran</td>
+							<td class="td-value">
+								<el-tag
+									size="small"
+									:type="statuses_pembayaran[data.status_pembayaran].type"
+								>{{statuses_pembayaran[data.status_pembayaran].label}}</el-tag>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</el-col>
+		</el-row>
 
-        <el-tabs type="border-card">
-            <el-tab-pane label="SUMMARY ITEM">
-                <table class="table table-sm table-bordered table-striped">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th class="text-center">Kategori</th>
-                            <th class="text-center">Berat</th>
-                            <th class="text-center">Terjual</th>
-                            <th class="text-center">Sisa</th>
-                            <th class="text-center">Dijual</th>
-                            <th class="text-center">Price Per Kg</th>
-                            <th class="text-center">Value</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="(item, index) in data.summaryItems" :key="index">
-                            <td>{{index+1}}.</td>
-                            <td>{{item.kategori}}</td>
-                            <td class="text-right">{{item.berat.toFixed(4) | formatNumber}} kg</td>
-                            <td class="text-right">{{item.terjual.toFixed(4) | formatNumber}} kg</td>
-                            <td class="text-right">{{(item.berat - item.terjual).toFixed(4) | formatNumber}} kg</td>
-                            <td class="text-right">{{item.berat_dijual.toFixed(4) | formatNumber}} kg</td>
-                            <td class="text-right">Rp {{item.price_per_unit | formatNumber}}</td>
-                            <td class="text-right">Rp {{(item.berat_dijual * item.price_per_unit).toFixed(0) | formatNumber}}</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </el-tab-pane>
-            <el-tab-pane label="DETAIL ITEM">
-                <table class="table table-sm table-bordered">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th class="text-center">Kategori</th>
-                            <th class="text-center">Material</th>
-                            <th class="text-center">Material Description</th>
-                            <th class="text-center">Berat Diajukan (kg)</th>
-                            <th class="text-center">Berat Terjual (kg)</th>
-                            <th class="text-center">Sisa (kg)</th>
-                            <th class="text-center">Berat Dijual (kg)</th>
-                            <th class="text-center">Price Per Unit (Rp)</th>
-                            <th class="text-center">Value (Rp)</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="(item, index) in data.items_wp" :key="index">
-                            <td>{{index+1}}.</td>
-                            <td>{{item.kategori}}</td>
-                            <td>{{item.material_id}}</td>
-                            <td>{{item.material_description}}</td>
-                            <td class="text-right">{{item.berat | formatNumber}} kg</td>
-                            <td class="text-right">{{item.terjual | formatNumber}} kg</td>
-                            <td class="text-right">{{item.berat - item.terjual | formatNumber}} kg</td>
-                            <td class="text-right">{{item.berat_dijual | formatNumber}} kg</td>
-                            <td class="text-right">Rp {{item.price_per_unit | formatNumber}}</td>
-                            <td class="text-right">Rp {{item.value.toFixed(0) | formatNumber}}</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </el-tab-pane>
-            <el-tab-pane label="PEMBAYARAN">
-                <Pembayaran :data="data.pembayaran" />
-            </el-tab-pane>
-        </el-tabs>
-    </div>
+		<el-tabs type="border-card">
+			<el-tab-pane label="SUMMARY ITEM">
+				<table class="table table-sm table-bordered table-striped">
+					<thead>
+						<tr>
+							<th>#</th>
+							<th class="text-center">Kategori</th>
+							<th class="text-center">Berat</th>
+							<th class="text-center">Terjual</th>
+							<th class="text-center">Sisa</th>
+							<th class="text-center">Dijual</th>
+							<th class="text-center">Price Per Kg</th>
+							<th class="text-center">Value</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr v-for="(item, index) in data.summaryItems" :key="index">
+							<td>{{index+1}}.</td>
+							<td>{{item.kategori}}</td>
+							<td class="text-right">{{Number(item.berat).toFixed(4) | formatNumber}} kg</td>
+							<td class="text-right">{{Number(item.terjual).toFixed(4) | formatNumber}} kg</td>
+							<td
+								class="text-right"
+							>{{(Number(item.berat) - Number(item.terjual)).toFixed(4) | formatNumber}} kg</td>
+							<td class="text-right">{{Number(item.berat_dijual).toFixed(4) | formatNumber}} kg</td>
+							<td class="text-right">Rp {{Number(item.price_per_unit) | formatNumber}}</td>
+							<td
+								class="text-right"
+							>Rp {{(Number(item.berat_dijual) * Number(item.price_per_unit)).toFixed(0) | formatNumber}}</td>
+						</tr>
+					</tbody>
+				</table>
+			</el-tab-pane>
+			<el-tab-pane label="DETAIL ITEM">
+				<table class="table table-sm table-bordered">
+					<thead>
+						<tr>
+							<th>#</th>
+							<th class="text-center">Kategori</th>
+							<th class="text-center">Material</th>
+							<th class="text-center">Material Description</th>
+							<th class="text-center">Berat Diajukan (kg)</th>
+							<th class="text-center">Berat Terjual (kg)</th>
+							<th class="text-center">Sisa (kg)</th>
+							<th class="text-center">Berat Dijual (kg)</th>
+							<th class="text-center">Price Per Unit (Rp)</th>
+							<th class="text-center">Value (Rp)</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr v-for="(item, index) in data.items_wp" :key="index">
+							<td>{{index+1}}.</td>
+							<td>{{item.kategori}}</td>
+							<td>{{item.material_id}}</td>
+							<td>{{item.material_description}}</td>
+							<td class="text-right">{{item.berat | formatNumber}} kg</td>
+							<td class="text-right">{{item.terjual | formatNumber}} kg</td>
+							<td class="text-right">{{item.berat - item.terjual | formatNumber}} kg</td>
+							<td class="text-right">{{item.berat_dijual | formatNumber}} kg</td>
+							<td class="text-right">Rp {{item.price_per_unit | formatNumber}}</td>
+							<td class="text-right">Rp {{item.value.toFixed(0) | formatNumber}}</td>
+						</tr>
+					</tbody>
+				</table>
+			</el-tab-pane>
+			<el-tab-pane label="PEMBAYARAN">
+				<Pembayaran :data="data.pembayaran" />
+			</el-tab-pane>
+		</el-tabs>
+	</div>
 </template>
 
 <script>
-import Pembayaran from './Pembayaran'
+import Pembayaran from "./Pembayaran";
 
 export default {
-    components: { Pembayaran },
-    props: ['data'],
-    data() {
-        return {
-            statuses: [
-                {type: 'info', label: 'Draft', value: 0},
-                {type: 'warning', label: 'Submitted', value: 1},
-                {type: 'success', label: 'Approved', value: 2},
-            ],
-            statuses_pembayaran: [
-                {type: 'info', label: 'No Payment', value: 0},
-                {type: 'warning', label: 'Partial', value: 1},
-                {type: 'success', label: 'Paid', value: 2},
-            ]
-        }
-    }
-}
+	components: { Pembayaran },
+	props: ["data"],
+	data() {
+		return {
+			statuses: [
+				{ type: "info", label: "Draft", value: 0 },
+				{ type: "warning", label: "Submitted", value: 1 },
+				{ type: "success", label: "Approved", value: 2 },
+			],
+			statuses_pembayaran: [
+				{ type: "info", label: "No Payment", value: 0 },
+				{ type: "warning", label: "Partial", value: 1 },
+				{ type: "success", label: "Paid", value: 2 },
+			],
+		};
+	},
+};
 </script>
 
 <style scoped>
 .td-label {
-    width: 150px;
-    background-color: #333c58;
-    font-weight: bold;
-    color: #fff;
+	width: 150px;
+	background-color: #333c58;
+	font-weight: bold;
+	color: #fff;
 }
 
 .td-value {
-    background-color: #efefef;
+	background-color: #efefef;
 }
 </style>
--- resources/js/pages/InOutStockBb.vue
+++ resources/js/pages/InOutStockBb.vue
@@ -1,157 +1,177 @@
 <template>
-    <div>
-        <el-form :inline="true" class="form-right" @submit.native.prevent="() => { return }">
-            <el-form-item>
-                <el-date-picker
-                @change="requestData"
-                v-model="dateRange"
-                value-format="yyyy-MM-dd"
-                :clearable="false"
-                type="daterange"
-                range-separator="-"
-                start-placeholder="Dari"
-                end-placeholder="Sampai">
-                </el-date-picker>
-            </el-form-item>
-            <el-form-item style="margin-center:0;">
-                <el-input placeholder="Search" prefix-icon="el-icon-search" v-model="keyword" @change="requestData" clearable>
-                    <el-button @click="() => { page = 1; keyword = ''; requestData(); }" slot="append" icon="el-icon-refresh"></el-button>
-                </el-input>
-            </el-form-item>
-        </el-form>
+  <div>
+    <el-form :inline="true" class="form-right" @submit.native.prevent="() => { return }">
+      <el-form-item>
+        <el-date-picker
+          @change="requestData"
+          v-model="dateRange"
+          value-format="yyyy-MM-dd"
+          :clearable="false"
+          type="daterange"
+          range-separator="-"
+          start-placeholder="Dari"
+          end-placeholder="Sampai"
+        ></el-date-picker>
+      </el-form-item>
+      <el-form-item style="margin-center:0;">
+        <el-input
+          placeholder="Search"
+          prefix-icon="el-icon-search"
+          v-model="keyword"
+          @change="requestData"
+          clearable
+        >
+          <el-button
+            @click="() => { page = 1; keyword = ''; requestData(); }"
+            slot="append"
+            icon="el-icon-refresh"
+          ></el-button>
+        </el-input>
+      </el-form-item>
+    </el-form>
 
-        <el-table :data="paginatedData.data" stripe
-        :default-sort = "{prop: 'tanggal', order: 'descending'}"
-        height="calc(100vh - 385px)"
-        v-loading="loading"
-        style="border-top:1px solid #eee;"
-        @filter-change="filterChange"
-        @sort-change="sortChange">
-            <el-table-column type="index" width="50" :index="paginatedData.from"> </el-table-column>
-            <el-table-column prop="tanggal" width="100" label="Tanggal" sortable="custom">
-                <template slot-scope="scope">
-                    {{ scope.row.tanggal | readableDate }}
-                </template>
-            </el-table-column>
-            <el-table-column prop="no_sj" min-width="200" label="No. Surat Jalan" sortable="custom"></el-table-column>
+    <el-table
+      :data="paginatedData.data"
+      stripe
+      :default-sort="{prop: 'tanggal', order: 'descending'}"
+      height="calc(100vh - 385px)"
+      v-loading="loading"
+      style="border-top:1px solid #eee;"
+      @filter-change="filterChange"
+      @sort-change="sortChange"
+    >
+      <el-table-column type="index" width="50" :index="paginatedData.from"></el-table-column>
+      <el-table-column prop="tanggal" width="100" label="Tanggal" sortable="custom">
+        <template slot-scope="scope">{{ scope.row.tanggal | readableDate }}</template>
+      </el-table-column>
+      <el-table-column prop="no_sj" min-width="200" label="No. Surat Jalan" sortable="custom"></el-table-column>
 
-            <el-table-column prop="location.name"
-            min-width="130"
-            label="Lokasi"
-            column-key="location_id"
-            :filters="$store.state.locationList.map(l => { return {value: l.id, text: l.plant + ' - ' + l.name } })"
-            sortable="custom">
-            </el-table-column>
+      <el-table-column
+        prop="location.name"
+        min-width="130"
+        label="Lokasi"
+        column-key="location_id"
+        :filters="$store.state.locationList.map(l => { return {value: l.id, text: l.plant + ' - ' + l.name } })"
+      ></el-table-column>
 
-            <el-table-column
-            prop="lokasi_asal"
-            min-width="130"
-            label="Lokasi Asal"
-            column-key="lokasi_asal"
-            :filters="$store.state.locationList.map(l => { return {value: l.name, text: l.plant + ' - ' + l.name } })"
-            sortable="custom"></el-table-column>
+      <el-table-column
+        prop="lokasi_asal"
+        min-width="130"
+        label="Lokasi Asal"
+        column-key="lokasi_asal"
+        :filters="$store.state.locationList.map(l => { return {value: l.name, text: l.plant + ' - ' + l.name } })"
+      ></el-table-column>
 
-            <el-table-column
-            prop="barang.nama"
-            min-width="200"
-            label="Kategori Barang"
-            column-key="kategori_barang_id"
-            :filters="$store.state.kategoriBarangList.map(l => {return {text: l.kode + ' - ' + l.nama, value: l.id}})"
-            sortable="custom">
-                <template slot-scope="scope">
-                    {{ scope.row.barang.kode }} - {{ scope.row.barang.nama }}
-                </template>
-            </el-table-column>
+      <el-table-column
+        prop="barang.nama"
+        min-width="200"
+        label="Kategori Barang"
+        column-key="kategori_barang_id"
+        :filters="$store.state.kategoriBarangList.map(l => {return {text: l.kode + ' - ' + l.nama, value: l.id}})"
+      >
+        <template slot-scope="scope">{{ scope.row.barang.kode }} - {{ scope.row.barang.nama }}</template>
+      </el-table-column>
 
-            <el-table-column prop="stock_in" min-width="110" label="Stock In" sortable="custom" align="right" header-align="right">
-                <template slot-scope="scope">
-                    {{ scope.row.stock_in | formatNumber }}
-                </template>
-            </el-table-column>
-            <el-table-column prop="stock_out" min-width="110" label="Stock Out" sortable="custom" align="right" header-align="right">
-                <template slot-scope="scope">
-                    {{ scope.row.stock_out | formatNumber }}
-                </template>
-            </el-table-column>
-            <el-table-column prop="eun" label="Satuan" header-align="center" align="center"></el-table-column>
-        </el-table>
+      <el-table-column
+        prop="stock_in"
+        min-width="110"
+        label="Stock In"
+        sortable="custom"
+        align="right"
+        header-align="right"
+      >
+        <template slot-scope="scope">{{ scope.row.stock_in | formatNumber }}</template>
+      </el-table-column>
+      <el-table-column
+        prop="stock_out"
+        min-width="110"
+        label="Stock Out"
+        sortable="custom"
+        align="right"
+        header-align="right"
+      >
+        <template slot-scope="scope">{{ scope.row.stock_out | formatNumber }}</template>
+      </el-table-column>
+      <el-table-column prop="eun" label="Satuan" header-align="center" align="center"></el-table-column>
+    </el-table>
 
-        <br>
+    <br />
 
-        <el-pagination
-        @current-change="(p) => { page = p; requestData(); }"
-        @size-change="(s) => { pageSize = s; requestData(); }"
-        :page-size="pageSize"
-        background
-        layout="prev, pager, next, sizes, total"
-        :page-sizes="[10,25,50,100]"
-        :total="paginatedData.total">
-        </el-pagination>
-    </div>
+    <el-pagination
+      @current-change="(p) => { page = p; requestData(); }"
+      @size-change="(s) => { pageSize = s; requestData(); }"
+      :page-size="pageSize"
+      background
+      layout="prev, pager, next, sizes, total"
+      :page-sizes="[10,25,50,100]"
+      :total="paginatedData.total"
+    ></el-pagination>
+  </div>
 </template>
 
 <script>
-import moment from 'moment'
+import moment from "moment";
 
 export default {
-    data: function() {
-        return {
-            loading: false,
-            keyword: '',
-            page: 1,
-            pageSize: 10,
-            sort: 'tanggal',
-            order: 'descending',
-            filters: {},
-            paginatedData: {},
-            dateRange: [
-                moment().format('YYYY-MM-01'),
-                moment().format('YYYY-MM-DD'),
-            ]
-        }
-    },
-    methods: {
-        sortChange: function(column) {
-            if (this.sort !== column.prop || this.order !== column.order) {
-                this.sort = column.prop;
-                this.order = column.order;
-                this.requestData();
-            }
-        },
-        filterChange: function(f) {
-            let column = Object.keys(f)[0];
-            this.filters[column] = Object.values(f[column]);
-            this.page = 1
-            this.requestData();
-        },
-        requestData: function() {
-            let params = {
-                page: this.page,
-                keyword: this.keyword,
-                pageSize: this.pageSize,
-                sort: this.sort,
-                order: this.order,
-                range: this.dateRange
-            }
-
-            this.loading = true;
-            axios.get('/inOutStockBb', {params: Object.assign(params, this.filters)}).then(r => {
-                this.paginatedData = r.data
-            }).catch(e => {
-                this.$message({
-                    message: e.response.data.message || e.response.message,
-                    type: 'error',
-                    showClose: true
-                });
-            }).finally(() => {
-                this.loading = false
-            })
-        }
-    },
-    mounted: function() {
+  data: function() {
+    return {
+      loading: false,
+      keyword: "",
+      page: 1,
+      pageSize: 10,
+      sort: "tanggal",
+      order: "descending",
+      filters: {},
+      paginatedData: {},
+      dateRange: [moment().format("YYYY-MM-01"), moment().format("YYYY-MM-DD")]
+    };
+  },
+  methods: {
+    sortChange: function(column) {
+      if (this.sort !== column.prop || this.order !== column.order) {
+        this.sort = column.prop;
+        this.order = column.order;
         this.requestData();
-        this.$store.commit('getLocationList')
-        this.$store.commit('getKategoriBarangList')
+      }
+    },
+    filterChange: function(f) {
+      let column = Object.keys(f)[0];
+      this.filters[column] = Object.values(f[column]);
+      this.page = 1;
+      this.requestData();
+    },
+    requestData: function() {
+      let params = {
+        page: this.page,
+        keyword: this.keyword,
+        pageSize: this.pageSize,
+        sort: this.sort,
+        order: this.order,
+        range: this.dateRange
+      };
+
+      this.loading = true;
+      axios
+        .get("/inOutStockBb", { params: Object.assign(params, this.filters) })
+        .then(r => {
+          this.paginatedData = r.data;
+        })
+        .catch(e => {
+          this.$message({
+            message: e.response.data.message || e.response.message,
+            type: "error",
+            showClose: true
+          });
+        })
+        .finally(() => {
+          this.loading = false;
+        });
     }
-}
+  },
+  mounted: function() {
+    this.requestData();
+    this.$store.commit("getLocationList");
+    this.$store.commit("getKategoriBarangList");
+  }
+};
 </script>
